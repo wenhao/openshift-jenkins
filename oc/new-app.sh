e32bc967b04bb500c7f72821f6a9ab31a8f76f36
Hao Wen <wenhao@126.com>
2017-08-28 17:04:02 +0800

    [Wen Hao] - update dockerfile.

--- oc/new-app.sh
+++ oc/new-app.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 name=$1
-oc new-app $1
 
 if [ $# -eq 0 ]
   then
     echo "Parameters NAMESPACE, No arguments supplied"
 fi
 
-fucntion getParameters() {
+for parameter in "$@"
+do
+    echo "$parameter"
+done
+
+getParameters() {
 
 
 }
